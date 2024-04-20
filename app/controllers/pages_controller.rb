85f3a06166ff3999a0d811d07cfe4ea94c377522
Jonathan <36344723+JonJLopez@users.noreply.github.com>
2024-04-20 10:32:02 -0400

    make one rep max unique to user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ before_action :authenticate_user!, except:[:about]
   end
 
   def routine
-    @one_rep_max = OneRepMax.first
+    @one_rep_max = current_user.one_rep_maxes.first
     @deadlift1 = (@one_rep_max.DeadliftMax * 0.65 / 5.0).round * 5
     @deadlift2 = (@one_rep_max.DeadliftMax * 0.75 / 5.0).round * 5
     @deadlift3 = (@one_rep_max.DeadliftMax * 0.85 / 5.0).round * 5
@@ -27,7 +27,7 @@ before_action :authenticate_user!, except:[:about]
   end
 
   def routine2
-    @one_rep_max = OneRepMax.first
+    @one_rep_max = current_user.one_rep_maxes.first
     @deadlift1 = (@one_rep_max.DeadliftMax * 0.70 / 5.0).round * 5
     @deadlift2 = (@one_rep_max.DeadliftMax * 0.80 / 5.0).round * 5
     @deadlift3 = (@one_rep_max.DeadliftMax * 0.90 / 5.0).round * 5
@@ -46,7 +46,7 @@ before_action :authenticate_user!, except:[:about]
   end
 
   def routine3
-    @one_rep_max = OneRepMax.first
+    @one_rep_max = current_user.one_rep_maxes.first
     @deadlift1 = (@one_rep_max.DeadliftMax * 0.75 / 5.0).round * 5
     @deadlift2 = (@one_rep_max.DeadliftMax * 0.85 / 5.0).round * 5
     @deadlift3 = (@one_rep_max.DeadliftMax * 0.95 / 5.0).round * 5
@@ -65,7 +65,7 @@ before_action :authenticate_user!, except:[:about]
   end
 
   def routine4
-    @one_rep_max = OneRepMax.first
+    @one_rep_max = current_user.one_rep_maxes.first
     @deadlift1 = (@one_rep_max.DeadliftMax * 0.40 / 5.0).round * 5
     @deadlift2 = (@one_rep_max.DeadliftMax * 0.50 / 5.0).round * 5
     @deadlift3 = (@one_rep_max.DeadliftMax * 0.60 / 5.0).round * 5
